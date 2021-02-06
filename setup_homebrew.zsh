db9246ffa1876828af2ede821dddffa5b4c3a128
itsencrypted <49042388+itsencrypted@users.noreply.github.com>
2021-02-06 13:22:53 -0300

    Set global --no-quarantine for Homebrew casks

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -4,6 +4,13 @@ echo "\n<<< Starting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+#TODO: Keep an eye for a different '--no-quarantine' solution
+#Currently, you can't do 'brew bundle --not-quarantine as an option
+#It's currently exported in zshrc:
+#export HOMEBREW_CASK_OPTS="--no-quarantine"
+#https://github.com/Homebrew/homebrew-bundle/issues/474
+
+
 brew bundle --verbose 
 
 
